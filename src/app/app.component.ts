63ccda3249dc6e89eef4aaee7c2406367bee1141
Tyler Gardner <gard.tp@gmail.com>
2018-12-29 15:10:51 -0700

    array and click event sort of working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'todo-app';
+ todoArray=[]
+addTodo(value){
+    this.todoArray.push(value)
+    console.log(this.todos)
+  }
+
 }
