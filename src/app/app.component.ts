7b5cf3509bd2e0aaaadebf34e6b0ee395236bd38
Tyler Gardner <gard.tp@gmail.com>
2018-12-29 16:13:34 -0700

    blank white screen and not working something broke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,26 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent {
- todoArray=[]
-addTodo(value){
+  todoArray=[]
+  addTodo(value){
     this.todoArray.push(value)
   }
 
   deleteItem(todo){
-  for(let i=0 ;i<= this.todoArray.length ;i++){
-   if(todo== this.todoArray[i]){
-    this.todoArray.splice(i,1)
-}
-}
-}
+    for(let i=0 ;i<= this.todoArray.length ;i++){
+      if(todo== this.todoArray[i]){
+        this.todoArray.splice(i,1)
+      }
+    }
+  }
+
+  todoSubmit(value:any){
+      if(value!==""){
+     this.todoArray.push(value.todo)
+      // this.todoForm.reset()
+     }else{
+       alert('Field required **')
+     }
+
+   }
 }
