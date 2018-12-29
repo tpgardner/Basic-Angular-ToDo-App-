a3d5ba0ad1467608768dec766d2be46911c99163
Tyler Gardner <gard.tp@gmail.com>
2018-12-29 15:52:18 -0700

    taking a break

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,11 @@ addTodo(value){
     this.todoArray.push(value)
   }
 
+  deleteItem(todo){
+  for(let i=0 ;i<= this.todoArray.length ;i++){
+   if(todo== this.todoArray[i]){
+    this.todoArray.splice(i,1)
+}
+}
+}
 }
